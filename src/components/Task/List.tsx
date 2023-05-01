8a4f53f3c99da85c2f307cd957c23c34666c6a87
Caleb Smith <caleb@smithcaleb.com>
2023-05-01 15:36:36 -0500

    List all tasks available as well as permit editing and deletion

---
+++ src/components/Task/List.tsx
@@ -0,0 +1,45 @@
+import useTasks from "@/hooks/useTasks";
+import { Task } from "@/types/Task";
+
+type TaskListProps = {
+  tasks: Task[];
+  editTask: Function;
+  toggleEditMode: Function;
+  deleteTask: Function;
+  markTaskComplete: Function;
+};
+export default function TaskList(props: TaskListProps) {
+  return (
+    <>
+      {props.tasks.map((task: Task) => {
+        return (
+          <div key={task.id}>
+            {task.editing ? (
+              <input
+                type="text"
+                value={task.title}
+                onChange={(e: any) => props.editTask(e, task)}
+              />
+            ) : (
+              <p>{task.finished ? <s>{task.title}</s> : task.title}</p>
+            )}
+
+            <div>
+              {task.editing ? (
+                <button onClick={() => props.toggleEditMode(task)}>
+                  Accept
+                </button>
+              ) : (
+                <button onClick={() => props.toggleEditMode(task)}>Edit</button>
+              )}
+              <button onClick={() => props.deleteTask(task)}>Delete</button>
+              <button onClick={() => props.markTaskComplete(task)}>
+                Mark Complete
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
