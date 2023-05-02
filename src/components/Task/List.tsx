7e1b3783bee2cbcf06e1c660fb38d426bac75b1e
Caleb Smith <caleb@smithcaleb.com>
2023-05-02 15:33:03 -0500

    Add an input ref to the task list to handle focus of new tasks

--- src/components/Task/List.tsx
+++ src/components/Task/List.tsx
@@ -1,4 +1,7 @@
 import { Task } from "@/types/Task";
+import TaskCard from "./Card";
+import TaskNewCard from "./NewCard";
+import { Ref } from "react";
 
 type TaskListProps = {
   tasks: Task[];
@@ -6,38 +9,15 @@ type TaskListProps = {
   toggleEditMode: Function;
   deleteTask: Function;
   markTaskComplete: Function;
+  inputRef: Ref<HTMLInputElement>;
+  inputFocus: Function;
 };
 export default function TaskList(props: TaskListProps) {
   return (
     <>
+      <TaskNewCard inputRef={props.inputRef} inputFocus={props.inputFocus} />
       {props.tasks.map((task: Task) => {
-        return (
-          <div key={task.id}>
-            {task.editing ? (
-              <input
-                type="text"
-                value={task.title}
-                onChange={(e: any) => props.editTask(e, task)}
-              />
-            ) : (
-              <p>{task.finished ? <s>{task.title}</s> : task.title}</p>
-            )}
-
-            <div>
-              {task.editing ? (
-                <button onClick={() => props.toggleEditMode(task)}>
-                  Accept
-                </button>
-              ) : (
-                <button onClick={() => props.toggleEditMode(task)}>Edit</button>
-              )}
-              <button onClick={() => props.deleteTask(task)}>Delete</button>
-              <button onClick={() => props.markTaskComplete(task)}>
-                Mark Complete
-              </button>
-            </div>
-          </div>
-        );
+        return <TaskCard task={task} {...props} />;
       })}
     </>
   );
